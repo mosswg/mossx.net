194b7cc21dc921ff3e02da4ccc5b505d82d8334c
Moss Gallagher <moss@mossx.net>
2021-09-14 16:43:20 -0400

    Added Basic Menu to Index.html
    Added a basic menu system to index.html using bootstrap for styling and
    JavaScript for the animation

---
+++ Menu.js
@@ -0,0 +1,48 @@
+
+
+const menu_list_id = "menu_list";
+
+var menu_amimation = null;
+var menu_is_open = false;
+var menu_is_chaging = false;
+var menu_elements = document.getElementsByClassName("menu_item");
+var current_menu_index = 0;
+        
+
+function toggleMenu() {
+
+    if (!menu_is_chaging) {
+        if (menu_is_open) {
+            menu_is_chaging = true;
+            menu_amimation = setInterval(closeMenu, 100);
+            menu_is_open = false;
+        }   
+        else {        
+            menu_is_chaging = true;
+            menu_amimation = setInterval(openMenu, 100);
+            menu_is_open = true;
+        }
+    }
+
+}
+
+
+function openMenu() {
+    if (current_menu_index < menu_elements.length) {
+        menu_elements[current_menu_index++].style.left = "0%";
+    } else {
+        clearInterval(menu_amimation);
+        current_menu_index--;
+        menu_is_chaging = false;
+    }
+}
+
+function closeMenu() {
+    if (current_menu_index >= 0) {
+        menu_elements[current_menu_index--].style.left = "-150%";
+    } else {
+        clearInterval(menu_amimation);
+        current_menu_index++;
+        menu_is_chaging = false;
+    }
+}
