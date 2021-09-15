c3f0c3f609b26bf252b68f21ed1b6bea4bf58ed7
Moss Gallagher <moss@mossx.net>
2021-09-15 00:54:34 -0400

    Menu: Reformatted Menu System
    Changed Menu system to be class based. This is earier to read and more
    expandable

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,6 +8,8 @@ var menu_is_chaging = false;
 var menu = null;
 var menu_elements = document.getElementsByClassName("menu_collapsible");
 var current_menu_index = 0;
+
+
         
 function create_menu() {
 
@@ -29,102 +31,99 @@ function create_menu() {
     }
 }
 
-function create_menu_elements() {
-    var menu_list = document.createElement('div');  // main menu list div
-    menu_list.classList = "menu_list btn-group"; 
-    
-    
+class MenuElement {
+    constructor(text, file_name,  button_class, menu_order, alternative_file_name = null) {
+        this.text = text;
+        this.file_name = file_name;
+        this.menu_order = menu_order;
+        this.button_class = button_class;
+        this.alternative_file_name = alternative_file_name;
+    }
 
-    
+    create_elements() {
+        var menu_list = document.createElement('div');  // main menu list div
+        menu_list.classList = "menu_list btn-group";
+        
+        
+        var open_button = document.createElement('a');
+        open_button["role"] = "button";
+        open_button.text = "☰";
+        open_button.classList = this.button_class + "menu_static";
+        open_button.onclick = toggle_menu;
+        
+        menu_list.appendChild(open_button);
+
+        console.log(files);
+        console.log(this.menu_order);
+
+        var menu_item = document.createElement('a');
+        menu_item["role"] = "button";
+        menu_item.classList = this.button_class + "menu_collapsible";
+        for (i = 0; i < this.menu_order.length; i++) {
+            var menu_item = document.createElement('a');
+            menu_item["role"] = "button";
+            var file = files[this.menu_order[i]];
+            menu_item.classList = this.button_class + "menu_collapsible";
+            menu_item.text = file.text;
+            menu_item.href = file.alternative_file_name ? file.alternative_file_name : file.file_name;
+            menu_list.appendChild(menu_item);
+        }
+        for (i = 0; i < external_menu_elements.length; i++) {
+            var menu_item = document.createElement('a');
+            menu_item["role"] = "button";
+            var file = external_menu_elements[i];
+            menu_item.classList = this.button_class + "menu_collapsible";
+            menu_item.text = file.text;
+            menu_item.href = file.alternative_file_name ? file.alternative_file_name : file.file_name;
+            menu_list.appendChild(menu_item);
+        }
+
+        console.log(menu);
+
+        menu.appendChild(menu_list);
+    }
+}
+
+const files = [
+    new MenuElement("Home", "index", "btn btn-outline-secondary menu_item ", [1, 2, 3], ""),
+    new MenuElement("About Me", "about", "btn btn-outline-secondary menu_item ", [0, 2, 3]),
+    new MenuElement("Sierpinski's triangle", "btn btn-outline-light menu_item ", "sierpinski", [0, 2, 3]),
+    new MenuElement("Contact", "contact", "btn btn-outline-secondary menu_item ", [0, 1, 3])
+];
+const external_menu_elements = [
+    new MenuElement("Source", "https://github.com/mossx-dev/mossx.net", []),
+    new MenuElement("Activism", "https://www.thequeerkiwi.com", [])
+];
+
+function create_menu_elements() {
+
     var file_uri = document.baseURI.split('/');
     file_name = file_uri[file_uri.length-1].split('.')[0];
 
-
-    if (file_name == "sierpinski") {
-        var button_class = "btn btn-outline-light menu_item ";
+    for (i = 0; i < files.length; i++) {
+        if (file_name == files[i].file_name || (files[i].alternative_file_name && files[i].alternative_file_name == file_name)) {
+            files[i].create_elements();
+            break;
+        }
     }
-    else {
-        var button_class = "btn btn-outline-secondary menu_item ";
-    }
 
-    var open_button = document.createElement('a');
-    open_button["role"] = "button";
-    open_button.text = "☰";
-    open_button.classList = button_class + "menu_static";
-    open_button.onclick = toggleMenu;
-    
-    menu_list.appendChild(open_button);
-    
-    var menu_item = document.createElement('a');
-    menu_item["role"] = "button";
-    menu_item.classList = button_class + "menu_collapsible";
-    if (file_name != "index" && file_name != "") {
-        menu_item.text = "Home";
-        menu_item.href = "/";
-    }
-    else {
-        menu_item.text = "About Me";
-        menu_item.href = "about";
-    }
-    menu_list.appendChild(menu_item);
-
-    menu_item = document.createElement('a');
-    menu_item["role"] = "button";
-    menu_item.classList = button_class + "menu_collapsible";
-    if (file_name != "sierpinski") {
-        menu_item.text = "Sierpinski's Triangle";
-        menu_item.href = "sierpinski";
-    }
-    else {
-        menu_item.text = "About Me";
-        menu_item.href = "about";
-    }
-    menu_list.appendChild(menu_item);
-
-
-    menu_item = document.createElement('a');
-    menu_item["role"] = "button";
-    menu_item.classList = button_class + "menu_collapsible";
-    menu_item.text = "Source";
-    menu_item.href = "https://github.com/mossx-dev/mossx.net";
-    
-    menu_list.appendChild(menu_item);
-
-    menu_item = document.createElement('a');
-    menu_item["role"] = "button";
-    menu_item.classList = button_class + "menu_collapsible";
-    menu_item.text = "Activism";
-    menu_item.href = "https://www.thequeerkiwi.com";
-    
-    menu_list.appendChild(menu_item);
-
-
-
-    menu.appendChild(menu_list);           //appending the element
-
-    // <div class="menu_list btn-group">
-	// 		<a role="button" class="btn btn-outline-secondary menu_item menu_static" onclick="toggleMenu()">&#9776;</a>
-	// 		<a role="button" class="btn btn-outline-secondary menu_item menu_collapsible" href="about">About Me</a>
-	// 		<a role="button" class="btn btn-outline-secondary menu_item menu_collapsible" href="sierpinski">Sierpinski's Triangle</a>
-	// 		<a role="button" class="btn btn-outline-secondary menu_item menu_collapsible" href="https://github.com/mossx-dev/mossx.net">Source</a>
-	// 		<a role="button" class="btn btn-outline-secondary menu_item menu_collapsible" href="https://www.thequeerkiwi.com/">Activism</a>
-
-	// 	</div>
 }
 
 
-function toggleMenu() {
+
+
+function toggle_menu() {
 
     if (!menu_is_chaging) {
         if (menu_is_open) {
             menu_is_chaging = true;
-            menu_amimation = setInterval(closeMenu, 100);
+            menu_amimation = setInterval(close_menu, 100);
             menu_is_open = false;
         }   
         else {
             menu_is_chaging = true;
-            openMenu();
-            menu_amimation = setInterval(openMenu, 150);
+            open_menu();
+            menu_amimation = setInterval(open_menu, 150);
             menu_is_open = true;
         }
     }
@@ -132,7 +131,7 @@ function toggleMenu() {
 }
 
 
-function openMenu() {
+function open_menu() {
     if (current_menu_index < menu_elements.length) {
         menu_elements[current_menu_index++].style.left = "0%";
     } else {
@@ -142,7 +141,7 @@ function openMenu() {
     }
 }
 
-function closeMenu() {
+function close_menu() {
     if (current_menu_index >= 0) {
         menu_elements[current_menu_index--].style.left = "-100%";
     } else {
@@ -150,4 +149,65 @@ function closeMenu() {
         current_menu_index++;
         menu_is_chaging = false;
     }
+}
+
+function create_footer() {
+
+    var footer = document.createElement('footer');
+    footer.classList = "fixed-bottom d-flex flex-wrap justify-content-between align-items-center p-2 m-0";
+    footer.style = "background-color: var(--bs-gray-dark);"
+
+    var copyright = document.createElement('div');
+    copyright.classList = "col-md-4 d-flex align-items-center";
+
+    var copyright_text = document.createElement('span');
+
+    copyright_text.innerText = "© Moss Gallagher " + new Date().getFullYear();
+    copyright_text.classList = "text-muted";
+
+    copyright.appendChild(copyright_text);
+
+
+    var links = document.createElement('ul');
+    links.classList = "nav col-md-4 justify-content-end list-unstyled d-flex";
+
+    var github = document.createElement('li');
+    github.classList = "ms-3";
+
+    var github_link = document.createElement('a');
+    github_link.classList =  "text-muted";
+    github_link.href = "https://github.com/mossx-dev";
+
+    var github_img = document.createElement('img');
+    github_img.src = "logo_GitHub.png";
+    github_img.width = "24";
+
+    github_link.appendChild(github_img);
+    github.appendChild(github_link);
+
+
+    var stackoverflow = document.createElement('li');
+    stackoverflow.classList = "ms-3";
+
+    var stackoverflow_link = document.createElement('a');
+    stackoverflow_link.classList =  "text-muted";
+    stackoverflow_link.href = "https://stackoverflow.com/users/15068365/mossx";
+
+    var stackoverflow_img = document.createElement('img');
+    stackoverflow_img.src = "logo_stackoverflow.png";
+    stackoverflow_img.width = "24";
+
+    stackoverflow_link.appendChild(stackoverflow_img);
+    stackoverflow.appendChild(stackoverflow_link);
+
+
+    links.appendChild(github);
+    links.appendChild(stackoverflow);
+
+    footer.appendChild(copyright);
+    footer.appendChild(links);
+
+
+    document.getElementsByTagName("body")[0].appendChild(footer);
+
 }
